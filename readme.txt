4a27f616650c84e045dcd621491cc2dbefdf1a94
Lars Behnke <lars.behnke@gmx.net>
2012-02-07 17:36:02 +0100

    Added source plugin, configured assembly.

--- readme.txt
+++ readme.txt
@@ -1,15 +1,16 @@
 SOAP Runner
 -----------
 
-Author: Lars Behnke 
+Author  : Lars Behnke 
+Version : ${project.version}
 
 This tool was created to send a series of SOAP requests to an arbitrary server.
 It simply takes a dump file created by TCPMon and resends its content.
 The original server response (as captured by TCPMon) and the target server response are stored
 in a manner that enable one to perform a file/directory diff or compare.
 
-Build binary:
-$ mvn assembly:assembly
+Build binaries:
+$ mvn clean install assembly:single
 
 Display available options:
 $ java -jar soaprunner-all-1.0.jar --help
--- src/main/java/org/endiansummer/tools/soaprunner/SoapRunnerApp.java
+++ src/main/java/org/endiansummer/tools/soaprunner/SoapRunnerApp.java
@@ -20,6 +20,7 @@ package org.endiansummer.tools.soaprunner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -32,7 +33,9 @@ public class SoapRunnerApp
 
     public static void main(String[] args) 
     {
-        Log.line("SOAP Runner Copyright (C) 2012 Lars Behnke");
+        String version = getVersion();
+        
+        Log.line("SOAP Runner " + version + " Copyright (C) 2012 Lars Behnke");
         Log.line("This program comes with ABSOLUTELY NO WARRANTY.");
         Log.line("This is free software, and you are welcome to redistribute it.");
         Log.line("under certain conditions. See license.txt for details.");
@@ -104,6 +107,21 @@ public class SoapRunnerApp
             System.exit(1);
         } 
     
+    }
+
+    private static String getVersion() 
+    {
+        Properties props = new Properties();
+        try
+        {
+            props.load(SoapRunnerApp.class.getResourceAsStream("/app.properties"));
+        }
+        catch (Exception e)
+        {
+            Log.err("Cannot load version.");
+        }
+        String version = props.getProperty("version");
+        return version == null ? "" : version;
     }
 
     private static Options buildOptions()
