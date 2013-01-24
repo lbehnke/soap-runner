901b6172b0f1059ce1e9782c79c57d73f7e1255a
Lars Behnke <lars.behnke@gmx.net>
2013-01-24 08:53:03 +0100

    License

--- src/test/java/org/endiansummer/tools/soaprunner/ReplacementProcessorTest.java
+++ src/test/java/org/endiansummer/tools/soaprunner/ReplacementProcessorTest.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Lars behnke.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Lars behnke - initial API and implementation
+ ******************************************************************************/
+
 package org.endiansummer.tools.soaprunner;
 
 import static org.junit.Assert.assertEquals;
